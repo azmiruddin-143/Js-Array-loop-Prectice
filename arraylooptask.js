c0c945946523c465f59e9318b2dce2e870de013b
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-09-06 16:00:43 +0600

    first commit

---
+++ arraylooptask.js
@@ -0,0 +1,74 @@
+
+// Array Loop Task//
+
+// node arraylooptask.js //
+
+//  Task Number 1 //
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+// const repet = colors.reverse();
+// console.log(repet);
+
+
+// node arraylooptask.js //
+
+//  Task Number 2 //
+
+
+// first way//
+// let numbers = [12, 98, 5, 41, 23, 78, 46, 85, 100, 96];
+// let azmir = [];
+// for (let even of numbers) {
+//     if (even % 2 === 0) {
+//         console.log(even);
+//         azmir.push(even)
+//     }
+// }
+
+// second way//
+// node arraylooptask.js //
+
+// let numbers2 = [12, 98, 8, 5, 10000, 200, 5550, 46, 85, 100, 96];
+
+
+// for (let i = 0; i < numbers2.length; i++) {
+
+//     if (numbers2[i] % 2 === !0) {
+//          console.log(numbers2[i]);
+//     }else{
+//         console.log("Akhaner sob jor sonkha na");
+//     }
+// }
+
+
+
+// node arraylooptask.js //
+
+//  Task Number 3 //
+
+// first way
+
+// const num = ['Tom', 'Tim', 'Tin', 'Tik']
+// let sti = "";
+
+// for (let st of num) {
+//     sti += st;
+// }
+// console.log(sti);
+
+// second waty
+
+// node arraylooptask.js //
+
+// const person = ['Tom', 'Tim', 'Tin', 'Tik','tok']
+// let arry = [];
+
+// for(let sti of person){
+//     arry.push(sti);
+// }
+//  let finalstring = arry.join('');
+//  console.log(finalstring);
+ 
+
+// node arraylooptask.js //
+
+//  Task Number 4 //
